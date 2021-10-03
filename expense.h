30eb014a2fa1ae931e091f3fb3a2330966656bbd
Christian Badoy <clbadoy@cpp.edu>
2021-10-03 15:59:38 -0700

    Modified allowed expenses for meals and taxis. Changed parameters for totalExpenses and allowableExpenses.

--- expense.h
+++ expense.h
@@ -1,9 +1,10 @@
 #ifndef EXPENSE_H
 #define EXPENSE_H
 
-float getTotalExpenses(float airfare, float carRental, float parkingFee, float conferenceFee, float hotelFee float mealFee);
+float getTotalExpenses(float airfare, float carRental, float privateVehicle, float parkingFee,
+                       float taxiFee, float conferenceFee, float hotelFee, float mealFee);
 
-float getTotalAllowableExpenses(float days);
+float getTotalAllowableExpenses(float days, float taxiDays, float arrive, float depart);
 
 float getTotalReimburse(float total, float allowable);
 
--- expenseFunctions.c
+++ expenseFunctions.c
@@ -1,23 +1,40 @@
 #include <stdio.h>
 #include "expense.h"
 
-float getTotalExpenses(float airfare, float carRental, float parkingFee, float conferenceFee, float hotelFee, float mealFee)
+float getTotalExpenses(float airfare, float carRental, float privateVehicle, float parkingFee,
+                       float taxiFee, float conferenceFee, float hotelFee, float mealFee)
 {
     float totalExpenses;
 
-    totalExpenses = airfare + carRental + parkingFee + conferenceFee + hotelFee;
+    totalExpenses = airfare + carRental + privateVehicle + parkingFee + taxiFee + conferenceFee + hotelFee + mealFee;
 
     return totalExpenses;
 }
 
-float getTotalAllowableExpenses(float days)
+float getTotalAllowableExpenses(float days, float taxiDays, float arrive, float depart)
 {
-    float allowExpenses, parkingWaive, taxiWaive, hotelWaive;
-    parkingWaive = 6;
-    taxiWaive = 10;
-    hotelWaive = 90;
+    float allowExpenses = 0, parkingWaive = 6, taxiWaive = 10, hotelWaive = 90, mealWaive = 0;
 
-    allowExpenses = days * (hotelWaive + taxiWaive + hotelWaive);
+    // Taxi Calculation
+    taxiWaive *= taxiDays;
+
+    // Meals Calculation Start
+    if (arrive <= 800)
+        mealWaive += 9;
+    else if (arrive > 800 && arrive <= 1300)
+        mealWaive += 12;
+    else if (arrive > 1300 && arrive <= 1900)
+        mealWaive += 16;
+
+    if (depart <= 700)
+        mealWaive += 9;
+    else if (depart > 700 && depart <= 1200)
+        mealWaive += 12;
+    else if (depart > 1200 && depart <= 1800)
+        mealWaive += 16;
+    // Meals Calculation End
+    
+    allowExpenses = days * (hotelWaive + parkingWaive) + taxiWaive + mealWaive;
 
     return allowExpenses;
 }
